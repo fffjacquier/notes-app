e1a72ee38b9be4f7d3b7bfdc2811c3e2c1f92940
Francois Jacquier <fjacquier@gmail.com>
2020-05-01 09:32:32 +0200

    zip: remove folders and file creation

--- pages/api/note/zip.js
+++ pages/api/note/zip.js
@@ -3,91 +3,43 @@ export default (req, res) => {
   const fs = require('fs')
   const path = require('path')
 
-  const deleteFolderRecursive = function (targetPath) {
-    if (fs.existsSync(targetPath)) {
-      fs.readdirSync(targetPath).forEach((file, index) => {
-        const curPath = path.join(targetPath, file)
-        if (fs.lstatSync(curPath).isDirectory()) {
-          // recurse
-          deleteFolderRecursive(curPath)
-        } else {
-          // delete file
-          fs.unlinkSync(curPath)
-        }
-      })
-      fs.rmdirSync(path)
-    }
-  }
-
   const ZLIB_COMPRESSION = 9
-  const zipPath = path.join(__dirname, '/notes.zip')
+  const zipPath = path.resolve(__dirname, 'output.zip')
 
   // read notes json file
   const notes = JSON.parse(req.body).notes
 
-  // for each folder create a directory
-  const folders = notes.reduce((acc, note) => {
-    if (acc.indexOf(note.folder) === -1) {
-      acc.push(note.folder)
-    }
-    return acc
-  }, [])
-  console.log(folders)
-
-  const cb = () => {
-    folders.forEach((folder) => {
-      fs.mkdir(
-        path.join(__dirname, 'zip', folder),
-        { recursive: true },
-        (err) => {
-          if (err) {
-            console.error('1', err)
-            throw 'Error!!' + err
-          }
-          createAndZip()
-        }
-      )
-    })
-  }
-
-  cb()
-
   // create archive and zip all
-  const createAndZip = () => {
-    notes.forEach((note) => {
-      fs.writeFileSync(
-        path.join(__dirname, 'zip', note.folder, note.id + '.txt'),
-        JSON.stringify(note.content)
-      )
-    })
+  res.setHeader('Content-Type', 'application/zip')
+  res.setHeader('Content-disposition', 'attachment; filename=notes.zip')
+  res.status(200)
 
-    const output = fs.createWriteStream(zipPath)
-    const archive = archiver('zip', {
-      zlib: { level: ZLIB_COMPRESSION },
-    })
+  // const output = fs.createWriteStream(path.join(__dirname, 'notes.zip'))
+  // Using output gives Error [ERR_STREAM_CANNOT_PIPE]: Cannot pipe, not readable
+  /*
+  output.on('close', () =>
+    console.log('Done!', archive.pointer() + ' total bytes')
+  )
+  */
+  const zip = archiver('zip', {
+    zlib: { level: ZLIB_COMPRESSION },
+  })
 
-    archive.on('error', (err) => {
-      throw err
-    })
+  zip.on('error', (err) => {
+    console.log('archive error', err)
+    throw err
+  })
+  zip.pipe(res)
 
-    output.on('close', () => {
-      console.log(`Total bytes: ${archive.pointer()}`)
-      console.log('archiving done!')
-    })
+  // add files and folder
+  notes.forEach((note) => {
+    zip.append(note.content, { name: note.folder + '/' + note.id + '.txt' })
+  })
+  zip.finalize()
 
-    archive.pipe(output)
+  // no download :(
 
-    // add files and folder
-    archive.directory('/zip')
-
-    archive.finalize()
-
-    // code to download zip file
-    /*res.setHeader('Content-Type', 'application/octet-stream')
-    res.setHeader('Content-Disposition', `attachment; filename=${zipPath}`)
-    res.setHeader('Content-Length', output.length)
-    res.send(output)
-    deleteFolderRecursive(path.join(__dirname, 'zip'))*/
-    res.end()
-  }
+  //res.pipe()
+  // .send(output)
+  res.end()
 }
